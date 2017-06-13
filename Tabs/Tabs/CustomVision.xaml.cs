7759f3319e6e7019d9023edb46bf17a6dded490d
David <voidnullbot@outlook.com>
2017-06-13 19:15:04 +1200

    handle exception when there is no internet

--- Tabs/Tabs/CustomVision.xaml.cs
+++ Tabs/Tabs/CustomVision.xaml.cs
@@ -56,6 +56,7 @@ namespace Tabs
 
         async Task MakePredictionRequest(MediaFile file)
         {
+            // An unhandled exception occured. occurred
             var client = new HttpClient();
 
             client.DefaultRequestHeaders.Add("Prediction-Key", "26e87165db3d41c58764deb3859c045f");
@@ -63,39 +64,48 @@ namespace Tabs
             string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/d163e053-5416-425f-86b6-c104f780d27f/image?iterationId=5048e35b-553d-4510-a7f8-e6116b9c0384";
 
             HttpResponseMessage response;
-
+            
             byte[] byteData = GetImageAsByteArray(file);
 
             using (var content = new ByteArrayContent(byteData))
             {
-
+                TagLabel.Text = "";
+                PredictionLabel.Text = "";
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-                response = await client.PostAsync(url, content);
-
-
-                if (response.IsSuccessStatusCode)
+                try
                 {
-                    var responseString = await response.Content.ReadAsStringAsync();
+                    response = await client.PostAsync(url, content);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var responseString = await response.Content.ReadAsStringAsync();
 
-                    JObject rss = JObject.Parse(responseString);
+                            JObject rss = JObject.Parse(responseString);
 
-                    //Querying with LINQ
-                    //Get all Prediction Values
-                    var Probability = from p in rss["Predictions"] select (string)p["Probability"];
-                    var Tag = from p in rss["Predictions"] select (string)p["Tag"];
+                            //Querying with LINQ
+                            //Get all Prediction Values
+                            var Probability = from p in rss["Predictions"] select (string)p["Probability"];
+                            var Tag = from p in rss["Predictions"] select (string)p["Tag"];
 
-                    //Truncate values to labels in XAML
-                    foreach (var item in Tag)
-                    {
-                        TagLabel.Text += item + ": \n";
-                    }
+                            //Truncate values to labels in XAML
+                            foreach (var item in Tag)
+                            {
+                                TagLabel.Text += item + ": \n";
+                            }
 
-                    foreach (var item in Probability)
-                    {
-                        PredictionLabel.Text += item + "\n";
-                    }
+                            foreach (var item in Probability)
+                            {
+                                PredictionLabel.Text += item + "\n";
+                            }
 
+                        }
                 }
+                catch (Exception)
+                {
+                    TagLabel.Text = "Cannot connect to the internet, please check your internet or try later";
+                }
+
+
+                
 
 
 
