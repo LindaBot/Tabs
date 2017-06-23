689c041a8abd0823292eb521b25e837723cc48fa
LindaBot <voidnullbot@outlook.com>
2017-06-23 21:55:24 +1200

    nothing

--- Tabs/Tabs.iOS/obj/iPhone/Debug/Tabs.iOS.c_.users.david.documents.visual_studio_2017.Projects.Tabs.Tabs.Tabs.CustomVision.xaml.g.cs
+++ Tabs/Tabs.iOS/obj/iPhone/Debug/Tabs.iOS.c_.users.david.documents.visual_studio_2017.Projects.Tabs.Tabs.Tabs.CustomVision.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace Tabs {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\David\\documents\\visual studio 2017\\Projects\\Tabs\\Tabs\\Tabs\\CustomVision." +
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\David\\Documents\\Visual Studio 2017\\Projects\\Tabs\\Tabs\\Tabs\\CustomVision." +
         "xaml")]
     public partial class CustomVision : global::Xamarin.Forms.ContentPage {
         
